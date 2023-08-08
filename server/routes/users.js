98602a55a85130bc857aa75d950ae1d62216957c
natthanon <takurokung55@gmail.com>
2023-08-08 11:36:34 +0700

    🔥Add new route

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,18 +7,21 @@ const {
   updateUsers,
   removeUsers,
   changeStatus,
+  changeRole,
 } = require("../controllers/users");
 
 const { tokenVerify, adminCheck } = require("../middleware/auth");
 
 router.get("/users", tokenVerify, adminCheck, listUsers);
 
-router.post("/users/change-role", tokenVerify, adminCheck, changeStatus);
+router.post("/users/change-status", tokenVerify, adminCheck, changeStatus);
+
+router.post("/users/change-role", tokenVerify, adminCheck, changeRole);
 
 router.get("/users/:id", readUsers);
 
 router.put("/users/:id", updateUsers);
 
-router.delete("/users/:id", removeUsers);
+router.delete("/users/:id", tokenVerify, adminCheck, removeUsers);
 
 module.exports = router;
