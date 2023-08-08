10e12105d5edfa2f56a45ab6a677f0dc08f18fa6
natthanon <takurokung55@gmail.com>
2023-08-08 11:37:50 +0700

    🔧Style Navbar look great

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -4,48 +4,147 @@ import {
   MailOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Menu } from "antd";
+import { Menu, Typography, Dropdown, Space, Button } from "antd";
+
+import {
+  ShoppingCartOutlined,
+  UserOutlined,
+  LoginOutlined,
+} from "@ant-design/icons";
 
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-const Navbar = () => {
+import { useSelector } from "react-redux";
+import { selectUser } from "../reducers";
+
+const AdminDropDown = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const items = [
     {
-      label: <Link to="/login">Login</Link>,
-      icon: <MailOutlined />,
-      key: "login",
-    },
-    {
-      label: <Link to="/register">Register</Link>,
-      key: "register",
-      icon: <AppstoreOutlined />,
-    },
-    {
-      label: "Logout",
-      key: "logout",
-      icon: <SettingOutlined />,
-      onClick: () => {
-        dispatch({ type: "LOGOUT", payload: null });
-        navigate("/");
-      },
+      label: (
+        <a
+          onClick={() => {
+            dispatch({ type: "LOGOUT", payload: null });
+            navigate("/");
+          }}
+        >
+          ออกจากระบบ
+        </a>
+      ),
+      key: "0",
     },
   ];
-
-  const [current, setCurrent] = useState("login");
-  const onClick = (e) => {
-    setCurrent(e.key);
-  };
   return (
-    <Menu
-      onClick={onClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      items={items}
-    />
+    <Dropdown
+      menu={{ items }}
+      trigger={["click"]}
+      placement="bottomRight"
+      arrow
+    >
+      <Link className="text-link">
+        <div className="user-info" style={{ display: "flex", gap: "10px" }}>
+          <UserOutlined />
+          <Typography>admin</Typography>
+        </div>
+      </Link>
+    </Dropdown>
+  );
+};
+
+const UserDropDown = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const items = [
+    {
+      label: (
+        <a
+          onClick={() => {
+            dispatch({ type: "LOGOUT", payload: null });
+            navigate("/");
+          }}
+        >
+          ออกจากระบบ
+        </a>
+      ),
+      key: "0",
+    },
+  ];
+  return (
+    <Dropdown
+      menu={{ items }}
+      trigger={["click"]}
+      placement="bottomRight"
+      arrow
+    >
+      <Link className="text-link">
+        <div className="user-info" style={{ display: "flex", gap: "10px" }}>
+          <UserOutlined />
+          <Typography>user</Typography>
+        </div>
+      </Link>
+    </Dropdown>
+  );
+};
+
+const NonLogin = () => {
+  const items = [
+    { label: <Link to="/login">เข้าสู่ระบบ</Link>, key: "0" },
+    { label: <Link to="/register">สมัครสมาชิก</Link>, key: "1" },
+  ];
+  return (
+    <Dropdown
+      menu={{ items }}
+      trigger={["click"]}
+      placement="bottomRight"
+      arrow
+    >
+      <Link className="text-link">
+        <div className="user-info" style={{ display: "flex", gap: "10px" }}>
+          <UserOutlined />
+        </div>
+      </Link>
+    </Dropdown>
+  );
+};
+
+const Navbar = () => {
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="navbar">
+      <ul className="links left">
+        <li>
+          <Link className="text-link texts">home</Link>
+        </li>
+        <li>
+          <Link className="text-link texts">product</Link>
+        </li>
+        <li>
+          <Link className="text-link texts">about</Link>
+        </li>
+      </ul>
+      <p className="title">Shop</p>
+      <ul className="links right">
+        <li>
+          <Link className="text-link">
+            <ShoppingCartOutlined />
+          </Link>
+        </li>
+        <li>
+          {user ? (
+            user.role === "admin" ? (
+              <AdminDropDown />
+            ) : (
+              <UserDropDown />
+            )
+          ) : (
+            <NonLogin />
+          )}
+        </li>
+      </ul>
+    </div>
   );
 };
 
