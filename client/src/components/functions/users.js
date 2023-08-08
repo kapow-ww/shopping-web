e963e0bb9f09d3abc11a79526126878c6746b894
natthanon <takurokung55@gmail.com>
2023-08-08 11:38:06 +0700

    🔥New axios path

--- client/src/components/functions/users.js
+++ client/src/components/functions/users.js
@@ -10,6 +10,18 @@ export const listUsers = async (authtoken) => {
 
 export const changeStatus = async (authtoken, value) => {
   return await axios.post(
+    import.meta.env.VITE_SERVER_API + "/users/change-status",
+    value,
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const changeRole = async (authtoken, value) => {
+  return await axios.post(
     import.meta.env.VITE_SERVER_API + "/users/change-role",
     value,
     {
@@ -18,4 +30,24 @@ export const changeStatus = async (authtoken, value) => {
       },
     }
   );
+};
+
+export const resetPassword = async (authtoken, id, value) => {
+  return await axios.put(
+    import.meta.env.VITE_SERVER_API + `/users/${id}`,
+    value,
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const removeUser = async (authtoken, id) => {
+  return await axios.delete(import.meta.env.VITE_SERVER_API + `/users/${id}`, {
+    headers: {
+      authtoken,
+    },
+  });
 };
