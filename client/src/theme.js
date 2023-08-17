b208bbbe824eb857de57e8bba2dd920a7c610c79
natthanon <takurokung55@gmail.com>
2023-08-17 16:00:53 +0700

    🔥Split theme config to new file

---
+++ client/src/theme.js
@@ -0,0 +1,20 @@
+export const themeConfig = {
+  token: {
+    colorPrimary: "#292929",
+    fontSize: 14,
+    borderRadius: 0,
+    wireframe: true,
+  },
+  components: {
+    Select: {
+      colorText: "rgba(0, 0, 0, 0.88)",
+      colorIconHover: "rgba(0, 0, 0, 0.88)",
+      colorPrimaryHover: "#363636",
+      colorPrimary: "#292929",
+      colorWarningHover: "#ffd666",
+      colorTextDescription: "rgba(0, 0, 0, 0.45)",
+      colorTextTertiary: "rgba(0, 0, 0, 0.45)",
+      controlItemBgActive: "#D8D9DA",
+    },
+  },
+};
